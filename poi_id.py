73637b55a27273515647b31cd6487bb7800438ed
redewe <Redewe@gmail.com>
2017-11-25 20:45:17 +1300

    Add files via upload

--- poi_id.py
+++ poi_id.py
@@ -19,7 +19,7 @@ from sklearn.svm import LinearSVC
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV, train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
 from time import time
@@ -28,11 +28,11 @@ from time import time
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'total_payments', 'total_stock_value', 'salary', 'deferral_payments', 'exercised_stock_options',
-                     'bonus', 'restricted_stock', 'restricted_stock_deferred',
-                     'expenses', 'loan_advances', 'other',  
-					 'director_fees', 'deferred_income',
-                     'long_term_incentive']
+features_list = ['poi', 
+				'total_payments', 'total_stock_value', 'salary', 'deferral_payments', 
+				'exercised_stock_options', 'bonus', 'restricted_stock', 
+				'restricted_stock_deferred', 'expenses', 'loan_advances', 'other',  
+				'director_fees', 'deferred_income','long_term_incentive']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "rb") as data_file:
@@ -140,13 +140,10 @@ for key, value in my_dataset.items():
 		my_dataset[key]['stock_value_ratio'] = ( float(my_dataset[key]['total_stock_value']) - float(my_dataset[key]['total_payments']))/float(my_dataset[key]['total_payments'])
 		
 
-# Add new feature to feature_list
-features_list.append('stock_value_ratio')														
-														
+		
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
-
 
 #scale data using MinMaxScaler
 
@@ -168,7 +165,7 @@ C_OPTIONS = [1, 10, 100, 1000]
 
 param_grid = [
     {
-        'reduce_dim': [PCA(iterated_power=7)],
+        'reduce_dim': [PCA()],
         'reduce_dim__n_components': N_FEATURES_OPTIONS,
         'classify__C': C_OPTIONS
     },
@@ -180,9 +177,34 @@ param_grid = [
 ]
 reducer_labels = ['PCA', 'KBest(chi2)']
 cv = StratifiedShuffleSplit(n_splits=100, random_state= 42)
+
+#Compare feature selection with Grid SearchCV and display chart
 grid = GridSearchCV(pipe, cv=cv, n_jobs=1, param_grid=param_grid)
 grid.fit(features_scaled, labels)
-print("Grid Search CV Best Params",grid.best_params_)
+print("Grid Search CV Best Params: ",grid.best_params_)
+
+#Generate mean scores
+mean_scores = np.array(grid.cv_results_['mean_test_score'])
+# scores are in the order of param_grid iteration, which is alphabetical
+mean_scores = mean_scores.reshape(len(C_OPTIONS), -1, len(N_FEATURES_OPTIONS))
+# select score for best C
+mean_scores = mean_scores.max(axis=0)
+bar_offsets = (np.arange(len(N_FEATURES_OPTIONS)) *
+               (len(reducer_labels) + 1) + .5)
+
+plt.figure()
+COLORS = 'bgrcmyk'
+for i, (label, reducer_scores) in enumerate(zip(reducer_labels, mean_scores)):
+    plt.bar(bar_offsets + i, reducer_scores, label=label, color=COLORS[i])
+
+plt.title("Comparing feature reduction techniques")
+plt.xlabel('Reduced number of features')
+plt.xticks(bar_offsets + len(reducer_labels) / 2, N_FEATURES_OPTIONS)
+plt.ylabel('Digit classification accuracy')
+plt.ylim((0, 1))
+plt.legend(loc='lower right')
+plt.savefig("Feature_Selection.png")
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -192,16 +214,15 @@ print("Grid Search CV Best Params",grid.best_params_)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 
-
 tree_dcf = tree.DecisionTreeClassifier(min_samples_split=2, random_state = 42)
 
 pipe_NB = Pipeline(memory=None, steps=[
-    ('PCA', SelectKBest(chi2)),
+    ('PCA', PCA(n_components=10)),
     ('classify_NB', GaussianNB())
 ])
 
 pipe_tree = Pipeline(memory=None, steps=[
-    ('KBest', SelectKBest(chi2)),
+    ('PCA', PCA(n_components=10)),
     ('classify_tree', tree_dcf)
 ])
 
@@ -213,38 +234,53 @@ pipe_tree = Pipeline(memory=None, steps=[
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
 features_scaled_train = scaler.fit_transform(features_train)
 features_scaled_test = scaler.fit_transform(features_test)
-	
-####### TESTING NB	
-t0 = time()
+
+### Evaluate effects of scaling with PCA
+#PCA before scaling
+pca = PCA(n_components=10)
+pca.fit(features_train, labels_train)
+pca.transform(features_test)
+
+# summarize components
+print("Explained Variance: ", pca.explained_variance_ratio_)  
+
+#PCA after scaling
+pca_new = PCA(n_components=10)
+pca_new.fit(features_scaled_train, labels_train)
+pca_new.transform(features_scaled_test)
+
+# summarize components
+print("Explained Variance (with scaled features): ", pca_new.explained_variance_ratio_)  
+print("##########################")
+
+### TEST NB SCORE WITH SCALED AND UNSCALED FEATURES
+pipe_NB_test = pipe_NB.fit(features_train, labels_train)
+pred_test = pipe_NB_test.predict(features_test)
+print("GNB Score with Unscaled Features: ", accuracy_score(pred_test, labels_test))
+
+
 pipe_NB.fit(features_scaled_train, labels_train)
-print("training time:", round(time()-t0, 3), "s")	
-
-t0 = time()
-pred = pipe_NB.predict(features_scaled_test)
-print("prediction time:", round(time()-t0, 3), "s")	
-
-acc = accuracy_score(pred, labels_test)
-print("GNB Score", acc)
+pred2 = pipe_NB.predict(features_scaled_test)
+print("GNB Score with Scaled Features: ", accuracy_score(pred2, labels_test))
 print("##########################")
 
 
-####### TESTING TREE
-t0 = time()
-pipe_tree.fit(features_scaled, labels)
-print("training time:", round(time()-t0, 3), "s")
-
-t0 = time()
-pred = pipe_tree.predict(features_scaled_test)
-print("prediction time:", round(time()-t0, 3), "s")	
-
+####### TESTING DECISION TREE NOW
+pipe_tree_test = pipe_tree.fit(features_train, labels_train)
+pred = pipe_tree_test.predict(features_test)
 acc = accuracy_score(pred, labels_test)
-print("DecisionTree Score", acc)
+print("DecisionTree Score with Unscaled features: ", acc)
+
+
+pipe_tree.fit(features_scaled_train, labels_train)
+pred = pipe_tree.predict(features_scaled_test)
+acc = accuracy_score(pred, labels_test)
+print("DecisionTree Score with Scaled features: ", acc)
 	
 print("##########################")
 	
@@ -277,13 +313,35 @@ y_true, y_pred = labels_test, clf.predict(features_scaled_test)
 print(classification_report(y_true, y_pred))
 
 
+#Test score with new feature
+features_list_new = features_list
+features_list_new.append('stock_value_ratio')	
+data2 = featureFormat(my_dataset, features_list_new, sort_keys = True)
+labels_new, features_new = targetFeatureSplit(data2)
+
+features_train_new, features_test_new, labels_train_new, labels_test_new = \
+    train_test_split(features_new, labels_new, test_size=0.3, random_state=42)
+
+features_scaled_train_new = scaler.fit_transform(features_train_new)
+features_scaled_test_new = scaler.fit_transform(features_test_new)
+							
+clf2 = GridSearchCV(
+	pipe_tree, 
+	cv=cv, 
+	param_grid=param_grid)
+clf2.fit(features_scaled_train_new, labels_train_new)
+clf2.predict(features_scaled_test_new)
+print("Grid Search CV Best Params - with new feature: ",clf2.best_params_)
+print("Grid Search CV Score - with new feature: ",clf2.score(features_scaled_test_new,labels_test_new ))
+y_true_new, y_pred_new = labels_test_new, clf2.predict(features_scaled_test_new)
+print(classification_report(y_true_new, y_pred_new))
+
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
-
+dump_classifier_and_data(clf2, my_dataset, features_list_new)
 
 
 
